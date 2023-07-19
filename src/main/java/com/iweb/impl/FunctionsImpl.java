74d491e5df093b1c9b25be6f1a1706f1c91c71c5
798408389 <798408389@qq.com>
2023-07-19 08:41:44 +0800

    测试，无改变

--- src/main/java/com/iweb/impl/FunctionsImpl.java
+++ src/main/java/com/iweb/impl/FunctionsImpl.java
@@ -171,8 +171,9 @@ public class FunctionsImpl implements Functions {
     public int manageAddress() {
         AddressDaoImpl addressDao = new AddressDaoImpl();
         LinkedList<Address> addressList = (LinkedList<Address>) addressDao.listAll();
+
         Scanner sc = new Scanner(System.in);
-        System.out.print("请选择地址ID:");
+        System.out.print("请选择地址ID: ");
         String choose = sc.nextLine();
         int res = Integer.parseInt(choose);
         for (Address a: addressList) {
